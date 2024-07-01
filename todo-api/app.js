c91284fd8f5922361aeacbc04566a8232adb4a05
chan000518 <128445671+chan000518@users.noreply.github.com>
2024-07-02 01:53:41 +0900

    실습2

--- todo-api/app.js
+++ todo-api/app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import tasks from "./data/mock.js"
 
 const app = express();
 
 app.get('/hello',(req,res) => {
     res.send('Hello Express!!!');
+});
+
+app.get('/tasks',(req,res) => {
+    res.send(tasks);
 });
 
 app.listen(3000, () => console.log('Server Started'))
