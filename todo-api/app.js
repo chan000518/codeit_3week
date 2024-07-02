436a66a07dbe292f082cccbea3b8bc44edcd5732
chan000518 <128445671+chan000518@users.noreply.github.com>
2024-07-02 12:38:35 +0900

    데이터베이스 연동 실습3

--- todo-api/app.js
+++ todo-api/app.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import tasks from "./data/mock.js"
+import mockTasks from "./data/mock.js"
 import mongoose from 'mongoose';
 import { DATABASE_URL } from './env.js';
+import Task from './models/Task.js';
 
 const app = express();
 app.use(express.json())
@@ -10,26 +11,22 @@ app.get('/hello',(req,res) => {
     res.send('Hello Express!!!');
 });
 
-app.get('/tasks',(req,res) => {
+app.get('/tasks', async (req,res) => {
     const sort = req.query.sort;
     const count = Number(req.query.count);
     
-    const compareFn = 
-        sort === "oldest"
-            ? (a,b) => a.createdAt - b.createdAt
-            : (a,b) => b.createdAt - a.createdAt
-    
-    let newTasks = tasks.sort(compareFn);
+    const sortOption = { createdAt: sort === 'oldest' ? 'asc' : 'desc'};
+    // 퀴리에 필터 조건을 여러개를 걸 수 있고
+    // await 이후엔 결과(객체)를 반환
+    const tasks = await Task.find().sort(sortOption).limit(count);
 
-    if (count){
-        newTasks = newTasks.slice(0, count);
-    }
-    res.send(newTasks);
+    res.send(tasks);
 });
 
-app.get("/tasks/:id",(req,res)=>{
-    const id = Number(req.params.id);
-    const task = tasks.find((task) => task.id === id);
+app.get("/tasks/:id", async (req,res)=>{
+    const id = req.params.id;
+    // 쿼리(데이터 베이스로 쿼리가 날라가고 이를 비동기로 기다림)
+    const task = await Task.findById(id);
     if(task){
         res.send(task);
     }else{
@@ -37,20 +34,14 @@ app.get("/tasks/:id",(req,res)=>{
     }
 })
 
-app.post("/tasks",(req,res)=>{
-    const newTask = req.body;
-    const ids = tasks.map((task)=> task.id)
-    newTask.id = Math.max(...ids) + 1
-    newTask.isComplete = false;
-    newTask.createdAt = new Date();
-    newTask.updatedAt = new Date();
-    tasks.push(newTask);
+app.post("/tasks",async (req,res)=>{
+    const newTask = await Task.create(req.body);
     res.status(201).send(newTask)
 })
 
 app.patch("/tasks/:id",(req,res)=>{
     const id = Number(req.params.id);
-    const task = tasks.find((task) => task.id === id);
+    const task = mockTasks.find((task) => task.id === id);
     if(task){
         Object.keys(req.body).forEach((key) =>{
             task[key] = req.body[key];
@@ -65,15 +56,14 @@ app.patch("/tasks/:id",(req,res)=>{
 
 app.delete("/tasks/:id",(req,res)=>{
     const id = Number(req.params.id);
-    const Idx = tasks.findIndex((task) => task.id === id);
+    const Idx = mockTasks.findIndex((task) => task.id === id);
     if(Idx >= 0 ){
-        tasks.splice(Idx, 1);
+        mockTasks.splice(Idx, 1);
         res.sendStatus(204);
     }else{
         res.status(404).send({ message : "id에 해당하는 내용 없음"})
     }
 })
-
 
 mongoose.connect(DATABASE_URL).then(() => console.log('Connected to DB'));
 
--- todo-api/models/Task.js
+++ todo-api/models/Task.js
@@ -4,6 +4,14 @@ const TaskSchema = new mongoose.Schema(
     {
         titile : {
             type: String,
+            require: true,
+            maxLength: 30,
+            validate:{
+                validator: function(title){
+                    return title.split(' ').length > 1;
+                },
+                message : "Must contain at least 2 단어."
+            }
         },
         description : {
             type: String,
