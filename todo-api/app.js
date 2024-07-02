7cc59af11ee41050902e54dd1aa0868b90d43156
chan000518 <128445671+chan000518@users.noreply.github.com>
2024-07-02 17:16:15 +0900

    데이터베이스 연동 실습4

--- todo-api/app.js
+++ todo-api/app.js
@@ -7,6 +7,22 @@ import Task from './models/Task.js';
 const app = express();
 app.use(express.json())
 
+function asyncHandler(handler){
+    return async function(req,res) {
+        try{
+            await handler(req,res);
+        } catch (e) {
+            if (e.name === 'ValidationError'){
+                res.status(400).send({message : e.message})
+            }else if( e.name === 'castError'){
+                res.status(404).send({message: 'Cannot find'})
+            }else {
+                res.status(500).send({ message: e.message})
+            }
+        }
+    }
+}
+
 app.get('/hello',(req,res) => {
     res.send('Hello Express!!!');
 });
